bf488aa91b8d27fa894dfa086c8ca3bd2e91089f
Elias Ramirez <ramirezelias110@gmail.com>
2021-04-14 16:16:50 -0400

    models and migrations

---
+++ models/business.js
@@ -0,0 +1,29 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class business extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  business.init({
+    id: DataTypes.INTEGER,
+    name: DataTypes.STRING,
+    address: DataTypes.STRING,
+    description: DataTypes.STRING,
+    type: DataTypes.STRING,
+    img: DataTypes.STRING,
+    ownerId: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'business',
+  });
+  return business;
+};
