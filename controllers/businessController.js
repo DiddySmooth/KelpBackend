ccd286b651d4c50de660ba6eca9b59a0b42a9adc
Anel Tuyebayeva <aneltuyebayeva@Anels-MacBook-Pro.local>
2021-04-15 11:27:19 -0400

    started working on business controller

--- controllers/businessController.js
+++ controllers/businessController.js
@@ -2,4 +2,43 @@ const models = require('../models')
 const axios = require('axios')
 const businessController = {}
 
+const businessController = {}
+businessController.getAll = async (req,res) => {
+    try {
+        let business = await model.business.findAll()
+        res.json({business})  
+        
+    } catch (error) {
+        res.json({error})
+    }
+}
+
+
+
+businessController.create = async (req,res) => {
+    try {
+
+        let user = await model.user.findOne({
+            where: {
+                id: req.params.id
+            }
+        })
+
+        let newBusiness = await model.post.create({
+
+        name: req.body.name,
+        address: req.body.address,
+        description: req.body.description,
+        type: req.body.type,
+        img: req.body.img
+    
+        })
+        user.addBusiness(newBusiness)
+        res.json({business}) 
+    } catch (error) {
+        res.json({error})
+    }
+}
+
+
 module.exports = businessController
--- routes/businessRoutes.js
+++ routes/businessRoutes.js
@@ -3,6 +3,8 @@ const businessController = require('../controllers/businessController')
 const express = require('express')
 const businessRoutes = express.Router()
 
+businessRoutes.get('/', businessController.getAll)
+businessRoutes.post('/', businessController.getAll)
 
 
 
